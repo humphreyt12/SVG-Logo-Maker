886ed88cd156e1521a5051ef3f780aa3438f95c4
Tina Humphrey <humphrey14t@gmail.com>
2024-01-05 02:57:15 -0500

    made changes to javacript files

--- Develop/index.js
+++ Develop/index.js
@@ -3,6 +3,7 @@ const inquirer = require('inquirer');
 const jest = require('jest');
 const fs = require('fs');
 
+
 const {Circle, Square, Triangle} = require("./lib/shapes");
 // Defines a Svg class that has a constructor with three methods for rendering and setting the text and shape elements in the SVG string.
 class Svg{
@@ -20,7 +21,8 @@ class Svg{
     setShapeElement(shape){
         this.shapeElement = shape.render()
 
-    }   
+    }
+   
 }
 // Defines array of 'questions' using the 'inquirer' library with the following questions.
 const questions = [
@@ -29,8 +31,10 @@ const questions = [
         name: "text",
         message: "TEXT: Enter up to (3) Characters:",
         validate: function (answer) {
-            if (answer.length < 4){
+            if (answer.length >= 4) {
                 return console.log("Invalid user text field detected! Please enter 1-3 Characters, no more and no less");
+            }else{
+                return true
             }
         }
     },
@@ -44,7 +48,25 @@ const questions = [
         name: "pixel-shape",
         message: "Choose which Pixel Shape you would like?",
         choices: ["Circle", "Square", "Triangle"],
-        
+        validate: function (answer) {
+            let user_shape;
+	        if (user_shape_type === "Square" || user_shape_type === "square") {
+		    user_shape = new Square();
+		    console.log("User selected Square shape");
+	        }   
+	        else if (user_shape_type === "Circle" || user_shape_type === "circle") {
+		    user_shape = new Circle();
+		    console.log("User selected Circle shape");
+	        }
+	        else if (user_shape_type === "Triangle" || user_shape_type === "triangle") {
+		    user_shape = new Triangle();
+		    console.log("User selected Triangle shape");
+	        }
+	        else{
+		    console.log("Invalid shape!");
+	        }
+          return true
+        }
     },
     {
         type: "input",
@@ -53,6 +75,8 @@ const questions = [
         validate: function (answer) {
             if (answer.length < 1) {
                 return console.log("A valid shape color is required.")
+            }else{
+                return true
             }
         }
     },
@@ -73,37 +97,34 @@ function init() {
     inquirer
     .prompt(questions)
     .then((answers) => {
-      const createLogo =  (answers);
-      
-      fs.writeFile(fileName, svg, (err) =>
-        err ? console.log(err) : console.log('You successfully generated a logo.svg!')
-      );
-    });
-  }
-  
+        var svgString = "";
+        var svg_file = "logo.svg";
+    //user text
+    user_text = answers["text"]
+
+    //user font color
+	user_font_color = answers["text-color"];
+
+    //user shape type
+	user_shape_type = answers["pixel-shape"];
+
+    //user shape color
+    user_shape_color = answers["shape-color"];
+
+	// Create a new Svg instance and add the shape and text elements to it
+	var svg = new Svg();
+	svg.setTextElement(user_text, user_font_color);
+    svg.setShapeElement(user_shape_type);
+	svgString = svg.render();
+	
+	//Print shape to log
+	console.log("Displaying shape:\n\n" + svgString);
+	//document.getElementById("svg_image").innerHTML = svgString;
+
+	console.log("Shape generation complete!");
+	console.log("Writing shape to file...");
+	writeToFile(svg_file, svgString); 
+}
+)}  
   // Function call to initialize app
   init();
-//     function init() {
-//         console.log("Starting init");
-//         var svgString = "";
-//         var svg_file = "logo.svg";
-    
-//         // Prompt the user for answers
-//         const answers = inquirer.prompt(questions);
-
-
-// 	// Create a new Svg instance and add the shape and text elements to it
-// 	var svg = new Svg();
-// 	svg.setTextElement(user_text, user_font_color);
-// 	svg.setShapeElement(user_shape);
-// 	svgString = svg.render();
-	
-// 	//Print shape to log
-// 	console.log("Displaying shape:\n\n" + svgString);
-// 	//document.getElementById("svg_image").innerHTML = svgString;
-
-// 	console.log("Shape generation complete!");
-// 	console.log("Writing shape to file...");
-// 	writeToFile(svg_file, svgString); 
-// }
-// init()
